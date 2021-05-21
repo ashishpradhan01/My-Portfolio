52683125f9df1ecf3de5831e5825a38d73731d4d
Ashish Pradhan <ashishprdhan892@gmail.com>
2021-05-21 13:20:00 +0530

    procfile-updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fs_%*j7*rp9da%e&pl)1+x1djp5_a3e=^3+0^imhzp8dhq21(-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'my-portfolio-online.herokuapp.com',
+    '127.0.0.1:8000',
+]
 
 
 # Application definition
@@ -127,5 +130,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
-#Production-level
+#Production-level  -- When putting this on S3 bucket change BASE_DIR to s3 bucket link so all staticfile collected in that bucket,
+# but for now we are collecting static files in django by using whitenoise to serve because django does not know how to serve these
+# files, when we are at production level it is recommended to use third party server to serve static files like AWS S3 bucket, but 
+# project we are using django to serve using white noise. 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
